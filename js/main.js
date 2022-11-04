6c73b08422b436b37fffb03a1f57af23f204fe7a
klen_dev <klenmrx@gmail.com>
2022-11-04 19:20:51 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// scroll animation
+
+window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 250) {
+      document.getElementById("header").classList.add("navbar-shrink");
+    } else {
+      document.getElementById("header").classList.remove("navbar-shrink");
+    }
+  }
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (document.documentElement.scrollTop > 450) {
+      document.getElementById("back-top").classList.add("d-flex");
+    } else {
+      document.getElementById("back-top").classList.remove("d-flex");
+    }
+  }
