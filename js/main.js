3d2609a0f11e06152538d81f4d67ace7f5339468
klen_dev <klenmrx@gmail.com>
2022-11-04 19:35:21 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -6,18 +6,9 @@ window.onscroll = function () {
   function scrollFunction() {
     if (document.documentElement.scrollTop > 250) {
       document.getElementById("header").classList.add("navbar-shrink");
-    } else {
-      document.getElementById("header").classList.remove("navbar-shrink");
-    }
-  }
-
-  window.onscroll = function () {
-    scrollFunction();
-  };
-  function scrollFunction() {
-    if (document.documentElement.scrollTop > 450) {
       document.getElementById("back-top").classList.add("d-flex");
     } else {
+      document.getElementById("header").classList.remove("navbar-shrink");
       document.getElementById("back-top").classList.remove("d-flex");
     }
   }
